b1a7b85cedafbe3e7cf0325dd1daed47e224220e
ShobujDas <shobujd6.gmail.com>
2024-04-06 00:35:46 +0600

    done full projects

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Logo from './Logo'
+import LanguageSwitcher from './LanguageSwitcher'
 
 
 function Navbar() {
   return (
     <nav className='py-4 md:py-6 border-b'>
       <div className="container mx-auto flex item-center justify-between">
-        <Logo/>
+        <Logo />
+        <LanguageSwitcher/>
       </div>
       
     </nav>
